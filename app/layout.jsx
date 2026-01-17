3c809469d96ddd14ef70544a497a9f8a0575a3b7
chaeyeon-jin <hcd0402@gmail.com>
2026-01-16 21:08:12 -0800

    analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import SmoothScroll from '../src/components/SmoothScroll'
+import Script from 'next/script'
 
 export const metadata = {
   title: "chaeyeonjin's portfolio",
@@ -13,6 +14,19 @@ export default function RootLayout({ children }) {
   return (
     <html lang="ko">
       <body className="font-nohemi">
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-BM3XP6WN5S"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-BM3XP6WN5S');
+          `}
+        </Script>
         <SmoothScroll>
           {children}
         </SmoothScroll>
